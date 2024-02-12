6991c54e751c390d00e78d1057af5f29bd3fbbc5
TheAmniel <theamniel@gmail.com>
2024-02-12 00:34:43 -0400

    Update deps for fix

--- spotify/spotify.go
+++ spotify/spotify.go
@@ -8,7 +8,7 @@ import (
 	"github.com/theamniel/spotify-server/config"
 	"github.com/theamniel/spotify-server/socket"
 	"github.com/zmb3/spotify/v2"
-	"github.com/zmb3/spotify/v2/auth"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
 	"golang.org/x/oauth2"
 )
 
